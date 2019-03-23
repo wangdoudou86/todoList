d4744545ddd451c6658ebc1e97a0bf14ac2ee8c9
krystal <892184375@qq.com>
2019-03-23 19:55:31 +0800

    提交

---
+++ src/components/todoItem.vue
@@ -0,0 +1,45 @@
+<template>
+ <div class="items">
+     <li>{{content}}</li>
+     <button @click="handleDelete">完成</button>
+ </div>
+</template>
+
+<script>
+export default {
+   props:['content','index'],
+   methods:{
+       handleDelete(){
+           this.$emit('deleteItems',this.index)
+       }
+   }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+*{
+    margin:0;padding:0;
+    box-sizing: border-box;
+  }
+  .items{
+    display:flex;
+    flex-direction: row;
+    margin-bottom:10px;
+    /* justify-content:center; */
+  }
+   li{
+    margin-right:5px;
+    font-size:14px;
+    line-height: 30px;
+  }
+  button{
+    border:none;
+    width:39px;
+    height:26px;
+    line-height: 30px;
+    border-radius:5px;
+    cursor: pointer;
+    text-align: center;
+  }
+</style>
---
+++ src/todoList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="todolist">
+    <input class="listName" type="text" v-model="inputValue" placeholder="添加事项" size="40"
+    onfocus="if(this.placeholder==='添加事项')this.placeholder=''"
+    onblur="if(this.placeholder=='')this.placeholder='添加事项'">
+    <button @click="handleSubmit">提交</button>
+    <ol>
+      <todo-item v-for="(item,index) in list"
+      :key="index"
+      :content="item"
+      :index="index"
+      @deleteItems="deleteItem"></todo-item>
+    </ol>
+  </div>
+</template>
+
+<script>
+import todoItem from './components/todoItem.vue'
+
+export default {
+  data(){
+    return {
+      inputValue:'',
+      list:[]
+    }
+  },
+  components: {
+    todoItem
+  },
+  methods:{
+    handleSubmit(){
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    deleteItem(index){
+      this.list.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+*{
+    margin:0;padding:0;
+    box-sizing: border-box;
+  }
+  body{
+    position: relative;
+
+  }
+  .todolist{
+    position: absolute;
+    left:50%;
+    top:20%;
+    margin-left:-180px;
+
+  }
+  input.listName{
+    height:30px;
+    line-height:30px;
+  }
+  button{
+    border:none;
+    width:45px;
+    height:30px;
+    margin-left:5px;
+    line-height:30px;
+    border-radius:5px;
+    cursor: pointer;
+  }
+  ol{
+    margin-top:15px;
+    margin-left:20px;
+  }
+</style>
